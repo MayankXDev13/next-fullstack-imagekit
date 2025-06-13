6bbd84932bdbf9d2f9f94ac1adfbd92db1bac025
Mayank <vijaymaynk2003@gmail.com>
2025-06-13 10:39:51 +0530

    Implement user registration endpoint with validation and error handling

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,4 +1,51 @@
-
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
 
+    if (!email || !password) {
+      return NextResponse.json(
+        {
+          error: "Email and password are required",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "User already registered",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await User.create({
+      email,
+      password,
+    });
+
+    return NextResponse.json(
+      { message: "User registered successfull" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Registration error", error);
+    return NextResponse.json(
+      { error: "Failed to register user" },
+      { status: 400 }
+    );
+  }
+}
