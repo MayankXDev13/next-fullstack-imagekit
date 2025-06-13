f8a23fc19c336230c4b0a40d1bf5228f68530921
Mayank <vijaymaynk2003@gmail.com>
2025-06-13 15:07:28 +0530

    Refactor video route to include session validation and error handling in POST request

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,6 +1,9 @@
+import { authOptions } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
-import Video from "@/models/Video";
-import { NextResponse } from "next/server";
+import Video, { IVideo } from "@/models/Video";
+
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
@@ -20,5 +23,52 @@ export async function GET() {
       },
       { status: 500 }
     );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized",
+        },
+        { status: 401 }
+      );
+    }
+
+    await connectToDatabase();
+
+    const body: IVideo = await request.json();
+
+    if (
+      !body.title ||
+      !body.description ||
+      !body.videoUrl ||
+      !body.thumbanilUrl
+    ) {
+      return NextResponse.json(
+        {
+          error: "Missing required fields",
+        },
+        { status: 400 }
+      );
+    }
+    const videoDate = {
+      ...body,
+      controls: body?.controls ?? true,
+      transformation: {
+        height: 1920,
+        width: 1080,
+        quality: body.transformation?.quality ?? 100,
+      },
+    };
+    const newVideo = await Video.create(videoDate);
+
+    return NextResponse.json(newVideo);
+  } catch (error) {
+    console.error("Failed to create video", error);
+    return NextResponse.json({ error: "Failed to create video" }, { status: 500 });
   }
 }
