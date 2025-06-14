95f454f2bbf5232124ea081cdb4acc12facb57dc
Mayank <vijaymaynk2003@gmail.com>
2025-06-14 08:02:30 +0530

    Implement LoginPage component with authentication handling and routing

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -8,10 +8,71 @@ function RegisterPage() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const router = useRouter();
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  
+    if (password !== confirmPassword) {
+      alert("Password do not match");
+      return;
+    }
 
-  return <div className="bg-amber-700">Register Page</div>;
+    try {
+      // react-query
+      // not mange loading, error debounce
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || "Registration failed");
+      }
+
+      console.log(data);
+      router.push("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+      <div>
+        <p>
+          Already have an account? <a href="/login">Login</a>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default RegisterPage;
